44785737aa5e7eb9bf5bb950daa572341fb3b96f
harry <harrymac1972@gmail.com>
2024-03-18 00:13:56 -0600

    server.js added

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+// connect with DataBase wrapper
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+});
