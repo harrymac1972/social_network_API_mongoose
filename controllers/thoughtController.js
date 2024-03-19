6eb22f96a3013c0ca39af9e3e0af41a7fb8bc81a
harry <harrymac1972@gmail.com>
2024-03-19 01:36:07 -0600

    thought.length checked added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,6 +7,9 @@ const thoughtController = {
   async getAllThoughts(req, res) {
     try {
       const thoughts = await Thought.find();
+      if (thoughts.length === 0) {
+        return res.status(404).json({ message: 'No thoughts...' });
+      }
       res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
