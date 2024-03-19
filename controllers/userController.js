6220c4908ac32f0d10d95956b564c5f01ab3c667
harry <harrymac1972@gmail.com>
2024-03-18 22:07:59 -0600

    userController.create(),update(),delete() added

--- controllers/userController.js
+++ controllers/userController.js
@@ -34,6 +34,47 @@ const userController = {
       res.status(500).json(err);
     }
   },
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).json(user);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+  async updateUser(req, res) {
+    try {
+        // WedDevSimp says use    User.findById().save()
+      const user = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!user) {
+        return res.status(404).json({ message: 'No such user' });
+      }
+      res.json(user);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+      if (!user) {
+        return res.status(404).json({ message: 'No such user' });
+      }
+      await User.updateMany(
+        { friends: req.params.userId },
+        { $pull: { friends: req.params.userId } }
+      );
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User deleted' });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  },
+
 }
 
 
