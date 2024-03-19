4862f4ac6a762b508a5ca849ce44d31f1519865d
harry <harrymac1972@gmail.com>
2024-03-18 19:00:13 -0600

    userController.getUserById() aded

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 
 
-const { User } = require('../models');
+const { Thought, User } = require('../models');
 const logError = require ('../utils/errorLogger');
 
 const userController = {
@@ -16,7 +16,24 @@ const userController = {
       logError(err);
       res.status(500).json(err);
     }
-  }
+  },  
+  async getUserById(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .populate('friends')
+        .populate('thoughts');
+      if (!user) {
+        return res.status(404).json({ message: 'No such user' });
+      }
+      res.json(user);
+    } catch (err) {
+      logError(err)
+      // console.log(error);
+      // console.log("\n========\n");
+      // console.log(error.message);
+      res.status(500).json(err);
+    }
+  },
 }
 
 
