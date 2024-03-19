6a7368a45887c371af19ad1e7ac222c731a661f3
harry <harrymac1972@gmail.com>
2024-03-19 03:55:50 -0600

    addReaction() added

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -84,6 +84,23 @@ const thoughtController = {
     }
   },
 
+  async addReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought exists with that ID' });
+      }
+      res.json(thought);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  },
+
 }
 
 
